deaa89b33aee4bc4241ace8f2a7fa26bbe4d88ca
Đỗ Phúc Đại <dophucdai3005@gmail.com>
2023-04-17 23:11:24 +0700

    Add project files.

---
+++ Muzique-Api/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Muzique_Api.Models
+{
+    public class AppSetting
+    {
+        public ConnectionStrings ConnectionStrings { get; set; }
+    }
+
+    public class ConnectionStrings
+    {
+        public string Default { get; set; }
+    }
+}
---
+++ Muzique-Api/Services/BaseService.cs
@@ -0,0 +1,30 @@
+﻿using MySqlConnector;
+using System;
+using System.Data;
+using Dapper;
+namespace Muzique_Api.Services
+{
+    public class BaseService
+    {
+        private const string _connectionString = "Server=127.0.0.1; User ID=root; Password=; Database=muzique";
+        protected IDbConnection _connection;
+        public BaseService() {
+            this._connection = new MySqlConnection(_connectionString);
+        }
+
+        public BaseService(IDbConnection _connection)
+        {
+            if (_connection == null)
+            {
+                this._connection = new MySqlConnection(_connectionString);
+            }
+            else
+                this._connection = _connection;
+        }
+
+        public static IDbConnection Connect()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
